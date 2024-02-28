cbbf7ad6fc10cc5de3f2a561247b6b28e59b04bc
Yahya Rajabi <rajabighulamyahya@gmail.com>
2024-02-28 11:46:43 -0700

    mixing

---
+++ useless1.m
@@ -0,0 +1,25 @@
+clear
+clearvars
+clc
+close all
+
+wingspan_values = 0.01:0.01:1.0;
+wing_planform_area_values = 0.01:0.01:1.0;
+
+% Generate combinations
+combinations = combvec(wingspan_values, wing_planform_area_values);
+
+combinations = combinations';
+
+% Calculate aspect ratio for each combination
+aspect_ratio = combinations(:,1).^2 ./ combinations(:,2);
+
+% Add aspect ratio as the third column
+combinations = [combinations, aspect_ratio];
+
+disp(combinations);
+
+a = [1,2,3];
+b= [4,5,6];
+comb = combvec(a,b);
+comb = comb';
